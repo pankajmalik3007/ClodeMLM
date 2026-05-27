f42ddfd426f3b110c24a8e599c18cbe4d1709832
pankajmalik3007 <pankajmalik3007@gmail.com>
2026-05-27 11:36:00 +0530

    File Rename

--- src/test/java/com/CloudeMLM_B01/LaunchTheWebsiteTest.java
+++ src/test/java/com/CloudeMLM_B01/LaunchTheWebsiteTest.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-public class LaunchTheWebsite {
+public class LaunchTheWebsiteTest {
 
 	@Test // This annotation tells Maven to run this method and generate reports!
 	public void testLoginPages() throws InterruptedException {
