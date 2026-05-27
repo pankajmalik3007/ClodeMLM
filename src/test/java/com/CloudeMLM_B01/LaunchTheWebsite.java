f7638a548cb718f9ffe7751c7b525b87a493c602
pankajmalik3007 <pankajmalik3007@gmail.com>
2026-05-27 11:30:19 +0530

    JUNIT Integration

--- src/test/java/com/CloudeMLM_B01/LaunchTheWebsite.java
+++ src/test/java/com/CloudeMLM_B01/LaunchTheWebsite.java
@@ -1,19 +1,23 @@
 package com.CloudeMLM_B01;
 
+import org.junit.jupiter.api.Test; // Import JUnit Test annotation
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class LaunchTheWebsite {
 
-	public static void main(String[] args) throws InterruptedException {
-		WebDriver driver = new ChromeDriver();
+	@Test // This annotation tells Maven to run this method and generate reports!
+	public void testLoginPages() throws InterruptedException {
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--headless=new"); // Runs smoothly in Jenkins background
+		
+		WebDriver driver = new ChromeDriver(options);
 		driver.manage().window().maximize();
 		driver.get("https://hub.trident1.dev/login");
 		
 		Thread.sleep(5000);
 		
 		driver.quit();
-
 	}
-
 }
